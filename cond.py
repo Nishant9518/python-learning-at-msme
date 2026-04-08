3e44546de46e0f680d56a4122032151cc43deb59
Nishant9518 <sharmaji9518220885@gmail.com>
2026-04-08 20:16:54 +0530

    learning python

---
+++ cond.py
@@ -0,0 +1,7 @@
+a = int(input("enter your age : "))
+if (a >0 and a <18):
+    print("you are a minor")
+elif (a >= 18 and a < 65):
+    print("you are an adult")
+else:
+    print("you are a senior citizen")

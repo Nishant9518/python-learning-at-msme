81cf44759acbf5d807b42d3c44aaeae1621957ed
Nishant9518 <sharmaji9518220885@gmail.com>
2026-04-13 12:53:25 +0530

    updated

---
+++ forloop.py
@@ -0,0 +1,10 @@
+name = " abhishek"
+for i in name :
+    print(i, end = "*")
+#a = ["apple", "mango", "orange"]
+#for i in a:
+ #   print(i)
+  #  for j in i:
+   #     print(j)
+#for i in range( 11):
+ #   print(i)
---
+++ new.py
@@ -0,0 +1,18 @@
+#for i in range(100):
+ #   if i % 2 == 0 and i % 3 == 0:
+  #      print(i)
+
+# a = input("enter your name :")
+#b = reversed(a)
+#print("reversed name is : ", end="")
+#for i in b:
+ #   print(i, end="")
+
+#for i in range(1, 101) :
+   # if i < 2 :
+    #    print(i)
+     #   continue
+    #for j in range(2, i):
+     #   if i % j == 0:
+      #      print(i)
+       #     break

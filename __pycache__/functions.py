c36f5d6e0ec78ab43d56f3b5fe7c7f4e34122319
Nishant9518 <sharmaji9518220885@gmail.com>
2026-04-15 22:06:41 +0530

    functions

---
+++ __pycache__/functions.py
@@ -0,0 +1,14 @@
+def name():
+    a = input("enter user name")
+    print("hello",a)
+   
+name()
+def addition():
+    a = int(input("enter first number : "))
+    b = int(input("enter second number : "))
+    add = a + b
+    print(add)
+    
+    
+addition()   
+

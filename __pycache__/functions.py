28cbc89979cb796ed9ed2727cc791d76729c8304
Nishant9518 <sharmaji9518220885@gmail.com>
2026-04-20 11:03:13 +0530

    arguments

--- __pycache__/functions.py
+++ __pycache__/functions.py
@@ -1,4 +1,4 @@
-def name():
+'''def name():
     a = input("enter user name")
     print("hello",a)
    
@@ -10,5 +10,21 @@ def addition():
     print(add)
     
     
-addition()   
+addition()
+#default arguments
+def multiplication(a=5,b=3):
+    mul = a * b
+    print(mul)
+multiplication()
+#variable length arguments
+def addition(*numbers):
+    sum = 0
+    for i in numbers:
 
+        sum += i 
+        print(sum/len(numbers))
+addition(5, 3, 2)
+#required arguments
+def name(fname,lname):
+    print("hello",fname,lname)
+name("Nishant","Bhardwaj")'''
